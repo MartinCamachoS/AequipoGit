6fe33a1d52c41f285f44d443b58011d908ea3149
dev-marieclaire <mail.maryjamesb@gmail.com>
2026-07-03 13:27:21 -0600

    no
    	modified:   Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,4 +8,5 @@ void main() {
     System.out.println("no w, porque yo digo w, como la vez");
 
     // Martin, cuánto es 110000 >> 11, pues es respuesta correcta como la vez, yo opino q frank zappa le gana a pues, pues gana
+    // Martin, cuánto es 110000 >> 11. No. La respuesta es 110
 }
